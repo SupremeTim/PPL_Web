b45d4472775056b7b078f2bf2f749664f89249ee
이승민 <lsm0341@naver.com>
2018-12-05 02:21:42 +0900

    template 끝~~

---
+++ PPL_template_1/web/js/color-pick.js
@@ -0,0 +1,10 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+setInterval(function(){ $(".navbar-fixed nav").css("background-color", getRandomColor()) }, 3000);
